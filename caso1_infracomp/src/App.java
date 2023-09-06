6338577b963e3802a427fe1a4d1a7af7bc84baf6
PaulaMolinaRuiz <pa.molina@uniandes.edu.co>
2023-09-06 17:07:37 -0500

    Pedir repartidores por consola

--- caso1_infracomp/src/App.java
+++ caso1_infracomp/src/App.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +19,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //int m=pedirRepartidores();
 
         for (int i= 0; i < 3; i++)
         {
@@ -27,7 +31,7 @@ public class App {
         Thread threadDespachador = new Thread(despachador);
 
         threadDespachador.start();
-        int m=3;
+        int m=5;
         for (int i= 0; i < m; i++)
         {
             Repartidor repartidor = new Repartidor(i, despacho);
@@ -39,7 +43,22 @@ public class App {
 
     }
 
+	public static synchronized int pedirRepartidores() {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			System.out.print("Ingrese la cantidad de Repartidores: ");
+			String numeroString = null;
+			try {
+				numeroString = br.readLine();
+			} catch (IOException e) {
 
+				e.printStackTrace();
+			}
+			int cantidadRepartidores = Integer.parseInt(numeroString);
+			return cantidadRepartidores;
+		}
+	}
     
 
 }
